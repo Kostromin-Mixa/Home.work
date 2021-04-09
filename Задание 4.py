9126846ad14ad8bef3900b41431bfec1e27245ad
Kostromin-Mixa <78191008+Kostromin-Mixa@users.noreply.github.com>
2021-04-09 15:45:10 +0500

    Add files via upload

--- Задание 4.py
+++ Задание 4.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python3
 import socket
-a = socket.gethostbyname('drive.google.com')
-b = socket.gethostbyname('mail.google.com')
-c = socket.gethostbyname('google.com')
-host_a = [a]
-host_ab = []
-if host_a != host_ab:
-    print("ERROR drive.google.com IP mismatch:", host_ab, a)
-else:
-    print("drive.google.com", a)
-host_ab = [a]
 
-host_b = [b]
-host_bb = []
-if host_b != host_bb:
-    print("ERROR mail.google.com IP mismatch:", host_bb, b)
-else:
-    print("mail.google.com", b)
-host_bb = [b]
+hosts = ['drive.google.com', 'mail.google.com', 'google.com']
+ips = {}
+for host in hosts:
+    ips[host] = socket.gethostbyname(host)
 
-host_c = [c]
-host_cb = []
-if host_c != host_cb:
-    print("ERROR google.com IP mismatch:", host_cb, c)
-else:
-    print("drive.google.com", c)
-host_cb = [c]
+while True:
+    for host in hosts:
+        oldip = ips[host]
+        newip = socket.gethostbyname(host)
+
+        if (oldip != newip):
+            print("ERROR", host, "IP mismatch:", oldip, newip)
+
+        else:
+            print(host, newip)
+    import time
+    time.sleep(3) # 3 seconds
+
+
+
